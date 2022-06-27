a9228da049f4a413550710651631d9ccfa2a6016
TP Honey <tp@harness.io>
2022-06-27 14:43:33 +0100

    (maint) change the release behaviour and binaries

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,10 +10,10 @@ set -e
 set -x
 
 # linux
-GOOS=linux GOARCH=amd64 go build -o release/linux/amd64/plugin
-GOOS=linux GOARCH=arm64 go build -o release/linux/arm64/plugin
+GOOS=linux GOARCH=amd64 go build -o release/linux-amd64-plugin
+GOOS=linux GOARCH=arm64 go build -o release/linux-arm64-plugin
 # mac
-GOOS=darwin GOARCH=amd64 go build -o release/darwin/amd64/plugin
-GOOS=darwin GOARCH=arm64 go build -o release/darwin/arm64/plugin
+GOOS=darwin GOARCH=amd64 go build -o release/darwin-amd64-plugin
+GOOS=darwin GOARCH=arm64 go build -o release/darwin-arm64-plugin
 # windows
-GOOS=windows go build -o release/windows/amd64/plugin.exe
+GOOS=windows GOARCH=amd64 go build -o release/plugin.exe
